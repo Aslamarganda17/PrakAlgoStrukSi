1039c43cf913145d8fb2678f0885eb6553d9712b
Aslam arganda <aslamcemplon@gmail.com>
2025-02-15 11:08:26 +0700

    Update and rename filesatu.txt to filesatu.cpp

---
+++ filesatu.cpp
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan1</title>
+</head>
+
+<body>
+    <h1 align="center"> Pratktikum si-i </h1>
+    <br> <br>
+    <p>Merapi adalah gunung berapi tipe stratovolcano (gunung api kerucut) yang terbentuk dari lapisan-lapisan lava, abu
+        vulkanik, dan batuan piroklastik. Kubah lavanya terus tumbuh dan runtuh secara periodik, menghasilkan awan panas
+        (pyroclastic flow) yang sangat berbahaya. Bentuknya yang kerucut dengan lereng curam merupakan ciri khas gunung
+        berapi aktif</p>
+    <font size="5" color="red"> LELAH YANG TERBAYARKAN </font>
+    <br>
+    <h2>TENTANG PENDAKIAN</h2>
+    <ul type="circle">
+        <li>c++</li>
+        <li>javascript</li>
+        <li>pyhon</li>
+    </ul>
+
+    <h2>untuk link</h2>
+    <a href="https://spada.upnyk.ac.id/my/" target="_blank">link spada</a>
+
+    <h2>Gambar gunung</h2>
+    <img width="800" height="500px" src="/foto/DSC03969.jpg" alt="gambar cantik">
+    <img width="300px" height="100px"
+        src="https://www.bing.com/images/search?view=detailV2&ccid=pIafm2O%2b&id=C787151BD41E0EC3E0B16FD71B41AA2772EA975E&thid=OIP.pIafm2O-b8uKSWErSq-NqQAAAA&mediaurl=https%3a%2f%2f1.bp.blogspot.com%2f-UMLmGoy_vrk%2fXxPCkfCxVUI%2fAAAAAAAAAX8%2fh2AAPBKIbPwc-ybAifRtUBrX7Tyvh5LKACLcBGAsYHQ%2fs1600%2fDSCF1545.jpg&exph=305&expw=474&q=gunung+merbabu&simid=608017033385289853&FORM=IRPRST&ck=56C485A2CF0F7CCCE5F61CE7A15E8340&selectedIndex=0&itb=0&ajaxhist=0&ajaxserp=0">
+
+    <table border="2">
+        <tr>
+            <th>no</th>
+            <th>peralatan</th>
+            <th>jumlah</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>headlamp</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td colspan="2">titipan</td>
+            <td>rRp 20.000</td>
+        </tr>
+    </table>
+
+    <form method="get" action="dashboard.html"></form>
+    <label for="nama"> Nama barang : </label>
+    <input type="password" name="nama"> <br>
+    <input type="checkbox" name="barang" value="trackingpole">
+    <input type="checkbox" name="barang" value="headlamp">
+    <input type="checkbox" name="barang" value="tenda">
+    <br>
+    <label for=""> pilih salah satu</label>
+    <input type="radio" name="pilihan" value="satu" satu>
+    <input type="radio" name="pilihan" value="dua" dua>
+
+    <br>
+    <input type="submit">
+    <input type="reset">
+    <form action=""></form>
+</body>
+
+</html>
