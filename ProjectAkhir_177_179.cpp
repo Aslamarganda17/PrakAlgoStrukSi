8e4498b969dbf1977a9d4cf5d183c2b4b963d789
nailafaiza <112226992+Nailafaiza@users.noreply.github.com>
2025-05-21 21:39:52 +0700

    Update ProjectAkhir_177_179.cpp

--- ProjectAkhir_177_179.cpp
+++ ProjectAkhir_177_179.cpp
@@ -6,6 +6,7 @@ using namespace std;
 struct Pesanan{
   int id;
   string namaPelanggan;
+  string makanan;
   int jumlah;
   int estimasiWaktu;
   Pesanan *next;
@@ -15,4 +16,21 @@ struct Pesanan{
 Pesanan *head = nullptr;
 Pesanan *tail = nullptr;
 
-//
+//variabel untuk menyimpan id pesanan selanjutnya
+int nextId = 1;
+
+//Fungsi untuk menyimpan data pesanan ke file data_pesanan.txt
+void simpanKeFile(Pesanan *head){
+  FILE *f = fopen("data_pesanan.txt", "w");
+  if (f==NULL){
+      cout << "Gagal membuka file untuk menyimpan data" << endl;
+      return;
+  }
+
+  Pesanan *bantu = head;
+  while (bantu != nullptr){
+      fprintf(f, "%d,%s,%s,%d,%d\n", bantu->id, bantu->namaPelanggan.c_str(), bantu->makanan.c_str(), bantu->estimasiWaktu);
+      bantu = bantu->next;
+  }
+  fclose(f);
+}
