060631c62ff460d78f6c0687e8c667092b8de4b2
ASLAM ARGANDA <aslamcemplon@gmail.com>
2025-05-22 11:01:03 +0700

    Update ProjectAkhir_177_179.cpp

--- ProjectAkhir_177_179.cpp
+++ ProjectAkhir_177_179.cpp
@@ -200,3 +200,15 @@ void tampilkanAntrian()
         bantu = bantu->next;
     }
 }
+
+// fungsi untuk mencari pesanan berdasarkan nama pelanggan (sequential search)
+void cariPesananNama()
+{
+    string cari; // untuk menyimpan input dari user saat user ingin mencari pesanan berdasarkan nama pelanggan
+    cout << "\nMasukkan nama pelanggan yang dicari: ";
+    cin.ignore();
+    getline(cin, cari);
+    Pesanan *bantu = head;
+    bool ditemukan = false;
+    while (bantu != nullptr)
+    {
